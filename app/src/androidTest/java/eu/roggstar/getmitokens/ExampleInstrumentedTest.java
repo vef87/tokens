09ecd0651df0f2bb3d2315aae9420eae90749677
vef87 <58501319+vef87@users.noreply.github.com>
2020-08-28 20:35:12 -0500

    Update test teamwork https://github.com/phrogg
    <?xml version="1.0" encoding="utf-8"?>
    <manifest xmlns:android="http://schemas.android.com/apk/res/android"
        package="eu.roggstar.getmitokens">
    
        <application
            android:allowBackup="true"
            android:icon="@mipmap/ic_launcher"
            android:label="@string/app_name"
            android:roundIcon="@mipmap/ic_launcher_round"
            android:supportsRtl="true"
            android:theme="@style/AppTheme">
            <activity android:name=".MainActivity">
                <intent-filter>
                    <action android:name="android.intent.action.MAIN" />
    
                    <category android:name="android.intent.category.LAUNCHER" />
                </intent-filter>
            </activity>
            <activity android:name=".VersionActivity"></activity>
        <?xml version="1.0" encoding="utf-8"?>
    <manifest xmlns:android="http://schemas.android.com/apk/res/android"
        package="eu.roggstar.getmitokens">
    
        <application
            android:allowBackup="true"
            android:icon="@mipmap/ic_launcher"
            android:label="@string/app_name"
            android:roundIcon="@mipmap/ic_launcher_round"
            android:supportsRtl="true"
            android:theme="@style/AppTheme">
            <activity android:name=".MainActivity">
                <intent-filter>
                    <action android:name="android.intent.action.MAIN" />
    
                    <category android:name="android.intent.category.LAUNCHER" />
                </intent-filter>
            </activity>
            <activity android:name=".VersionActivity"></activity>
        </application>
    
    </manifest>

--- app/src/androidTest/java/eu/roggstar/getmitokens/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/eu/roggstar/getmitokens/ExampleInstrumentedTest.java
@@ -24,3 +24,27 @@ public class ExampleInstrumentedTest {
         assertEquals("eu.roggstar.getmitoken", appContext.getPackageName());
     }
 }
+git remote add origin https://github.com/vef87/tokens.git
+git branch -M master
+git push -u origin masterreadme-edits <?xml version="1.0" encoding="utf-8"?>
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+    package="eu.roggstar.getmitokens">
+
+    <application
+        android:allowBackup="true"
+        android:icon="@mipmap/ic_launcher"
+        android:label="@string/app_name"
+        android:roundIcon="@mipmap/ic_launcher_round"
+        android:supportsRtl="true"
+        android:theme="@style/AppTheme">
+        <activity android:name=".MainActivity">
+            <intent-filter>
+                <action android:name="android.intent.action.MAIN" />
+
+                <category android:name="android.intent.category.LAUNCHER" />
+            </intent-filter>
+        </activity>
+        <activity android:name=".VersionActivity"></activity>
+    </application>
+
+</manifest>
